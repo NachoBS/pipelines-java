f0e6509cf617abd2bedd33545d3bc17852842dea
NachoBS <jignaciobs@gmail.com>
2021-04-20 22:01:58 +0200

    Update Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -2,6 +2,7 @@ package com.microsoft.demo;
 
 public class Demo {
     public void DoSomething(boolean flag){
+        System.out.println("Here I am!!");
         if(flag){
             System.out.println("I am covered");
             return;
